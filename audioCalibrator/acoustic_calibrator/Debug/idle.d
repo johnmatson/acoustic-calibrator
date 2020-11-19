d1c39083dc9d98593e930c2701aab4f6f3fa6c84
Armueller-604 <73917657+Armueller-604@users.noreply.github.com>
2020-11-18 18:18:33 -0800

    ADC functions added, deviceinit added

---
+++ audioCalibrator/acoustic_calibrator/Debug/idle.d
@@ -0,0 +1,165 @@
+# FIXED
+
+idle.obj: ../idle.c
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/std.h
+idle.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.8.LTS/include/stdarg.h
+idle.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.8.LTS/include/_ti_config.h
+idle.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.8.LTS/include/linkage.h
+idle.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.8.LTS/include/stddef.h
+idle.obj: C:/ti/bios_6_76_04_02/packages/ti/targets/std.h
+idle.obj: C:/ti/bios_6_76_04_02/packages/ti/targets/C28_large.h
+idle.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.8.LTS/include/stdint.h
+idle.obj: C:/ti/bios_6_76_04_02/packages/ti/sysbios/BIOS.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/xdc.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types__prologue.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/package/package.defs.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types__epilogue.h
+idle.obj: C:/ti/bios_6_76_04_02/packages/ti/sysbios/BIOS__prologue.h
+idle.obj: C:/ti/bios_6_76_04_02/packages/ti/sysbios/package/package.defs.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Error.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Error__prologue.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Main.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IModule.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IHeap.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IInstance.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IModule.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Error.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Memory.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IModule.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IHeap.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Error.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/package/Memory_HeapProxy.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IInstance.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IHeap.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Error.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IGateProvider.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IInstance.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IModule.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/package/Main_Module_GateProxy.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IInstance.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IGateProvider.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IModule.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Error__epilogue.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IHeap.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IModule.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IGateProvider.h
+idle.obj: C:/ti/bios_6_76_04_02/packages/ti/sysbios/package/BIOS_RtsGateProxy.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IInstance.h
+idle.obj: C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IGateProvider.h
+idle.obj: C:/ti/bios_6_76_04_02/packages/ti/sysbios/BIOS__epilogue.h
+idle.obj: C:/ti/bios_6_76_04_02/packages/ti/sysbios/package/BIOS_RtsGateProxy.h
+idle.obj: ../Peripheral_Headers/F2802x_Device.h
+idle.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.8.LTS/include/assert.h
+idle.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.8.LTS/include/stdbool.h
+idle.obj: ../Peripheral_Headers/F2802x_Adc.h
+idle.obj: ../Peripheral_Headers/F2802x_BootVars.h
+idle.obj: ../Peripheral_Headers/F2802x_DevEmu.h
+idle.obj: ../Peripheral_Headers/F2802x_Comp.h
+idle.obj: ../Peripheral_Headers/F2802x_CpuTimers.h
+idle.obj: ../Peripheral_Headers/F2802x_ECap.h
+idle.obj: ../Peripheral_Headers/F2802x_EPwm.h
+idle.obj: ../Peripheral_Headers/F2802x_Gpio.h
+idle.obj: ../Peripheral_Headers/F2802x_I2c.h
+idle.obj: ../Peripheral_Headers/F2802x_NmiIntrupt.h
+idle.obj: ../Peripheral_Headers/F2802x_PieCtrl.h
+idle.obj: ../Peripheral_Headers/F2802x_PieVect.h
+idle.obj: ../Peripheral_Headers/F2802x_Spi.h
+idle.obj: ../Peripheral_Headers/F2802x_Sci.h
+idle.obj: ../Peripheral_Headers/F2802x_SysCtrl.h
+idle.obj: ../Peripheral_Headers/F2802x_XIntrupt.h
+
+../idle.c: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/std.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.8.LTS/include/stdarg.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.8.LTS/include/_ti_config.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.8.LTS/include/linkage.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.8.LTS/include/stddef.h: 
+C:/ti/bios_6_76_04_02/packages/ti/targets/std.h: 
+C:/ti/bios_6_76_04_02/packages/ti/targets/C28_large.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.8.LTS/include/stdint.h: 
+C:/ti/bios_6_76_04_02/packages/ti/sysbios/BIOS.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/xdc.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types__prologue.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/package/package.defs.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types__epilogue.h: 
+C:/ti/bios_6_76_04_02/packages/ti/sysbios/BIOS__prologue.h: 
+C:/ti/bios_6_76_04_02/packages/ti/sysbios/package/package.defs.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Error.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Error__prologue.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Main.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IHeap.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IInstance.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Error.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Memory.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IHeap.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Error.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/package/Memory_HeapProxy.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IInstance.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IHeap.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Error.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IGateProvider.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IInstance.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/package/Main_Module_GateProxy.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IInstance.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IGateProvider.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Error__epilogue.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IHeap.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IGateProvider.h: 
+C:/ti/bios_6_76_04_02/packages/ti/sysbios/package/BIOS_RtsGateProxy.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IInstance.h: 
+C:/ti/xdctools_3_51_01_18_core/packages/xdc/runtime/IGateProvider.h: 
+C:/ti/bios_6_76_04_02/packages/ti/sysbios/BIOS__epilogue.h: 
+C:/ti/bios_6_76_04_02/packages/ti/sysbios/package/BIOS_RtsGateProxy.h: 
+../Peripheral_Headers/F2802x_Device.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.8.LTS/include/assert.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.8.LTS/include/stdbool.h: 
+../Peripheral_Headers/F2802x_Adc.h: 
+../Peripheral_Headers/F2802x_BootVars.h: 
+../Peripheral_Headers/F2802x_DevEmu.h: 
+../Peripheral_Headers/F2802x_Comp.h: 
+../Peripheral_Headers/F2802x_CpuTimers.h: 
+../Peripheral_Headers/F2802x_ECap.h: 
+../Peripheral_Headers/F2802x_EPwm.h: 
+../Peripheral_Headers/F2802x_Gpio.h: 
+../Peripheral_Headers/F2802x_I2c.h: 
+../Peripheral_Headers/F2802x_NmiIntrupt.h: 
+../Peripheral_Headers/F2802x_PieCtrl.h: 
+../Peripheral_Headers/F2802x_PieVect.h: 
+../Peripheral_Headers/F2802x_Spi.h: 
+../Peripheral_Headers/F2802x_Sci.h: 
+../Peripheral_Headers/F2802x_SysCtrl.h: 
+../Peripheral_Headers/F2802x_XIntrupt.h: 
